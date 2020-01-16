2b3418c18560382f29a02fb8ceac71dd631c0501
entityleak <entity.leak@gmail.com>
2020-01-16 19:03:37 +0100

    get books and sort by collection

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,69 @@ export default {
   components: {
 
   },
+  data(){
+    return{
+      loaded: false,
+      books: null,
+      collections: null
+    }
+  },
   methods:{
+
+    sortCollections(){
+
+      const collections = {}
+
+      for (var i = 0; i < this.books.length; i++) {
+        this.books[i]
+      }
+
+      this.books.forEach(function(book){
+        if (book.collections){
+          Object.entries(book.collections).forEach(function([collectionKey, collectionName]){
+            // console.log(i);
+            if (!collections[collectionKey]){
+              collections[collectionKey] = {
+                key : +collectionKey,
+                name : collectionName,
+                books : []
+              }
+            }
+
+            collections[collectionKey].books.push(+book.book_id);
+
+          })
+        }
+      })
+      // console.log(collections);
+
+      this.collections = Object.values(collections);
+    },
+
     getContent(){
-      this.$api.get('?userid=' + process.env.USERID + '&key=' + process.env.KEY )
+      this.$api.get('?userid=' + process.env.VUE_APP_USERID + '&key=' + process.env.VUE_APP_KEY +
+      '&max=3000' + '&showCollections=1' + '&showTags=1' + '&responseType=json' )
       .then((response) => {
         console.log(response.data);
+
+        this.books = Object.entries(response.data.books).map(([
+          book_id, bookData
+        ]) => {
+          return {
+            book_id,
+            title : bookData.title,
+            author_lf : bookData.author_lf,
+            author : bookData.author_fl,
+            isbn  : bookData.ISBN,
+            tags : bookData.tags,
+            collections : bookData.collections
+
+          }
+        });
+
+        this.sortCollections();
+
+        this.loaded = true;
       })
       .catch(e => console.log(e));
     }
