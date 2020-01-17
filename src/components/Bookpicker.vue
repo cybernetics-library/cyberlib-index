d1e39ea4c82ac86e01480d28779d3c1300d78f00
entityleak <entity.leak@gmail.com>
2020-01-17 15:27:09 +0100

    book finder setup

---
+++ src/components/Bookpicker.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+<ol>
+  <h2>Or find a book</h2><br>
+
+</ol>
+</template>
+
+<script>
+export default {
+  name: 'BookPicker',
+  props:{
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+ol{
+  /* background-color: white; */
+  /* padding: 5mm; */
+}
+
+
+</style>
