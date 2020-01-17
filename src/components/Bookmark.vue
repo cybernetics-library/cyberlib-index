0932d959ecd58e317eeaf71b17cbef0cb37b78b1
entityleak <entity.leak@gmail.com>
2020-01-17 09:18:22 +0100

    qr code generator

--- src/components/Bookmark.vue
+++ src/components/Bookmark.vue
@@ -1,21 +1,32 @@
 <template lang="html">
   <div>
+    <qrcode-vue :value="bookData.cyliburl"></qrcode-vue>
     <img :src='bookData.cover' />
     {{bookData.book_id}}
     {{bookData.title}},
     {{bookData.author}},
     {{bookData.publication_date}}
+    <div id='placeHolder'></div>
   </div>
 </template>
 
 <script>
+import QrcodeVue from 'qrcode.vue'
+
+
 export default {
   name: 'bookmark',
+  components:{
+    QrcodeVue
+  },
   props:{
     bookData:{
       type:Object,
       required:true
     }
+  },
+  created(){
+
   }
 }
 </script>
